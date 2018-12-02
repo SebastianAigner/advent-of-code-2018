46f32f04dd836522c9ee6273b4ba1b66641dc42f
Sebastian Aigner <sebastian.aigner@tum.de>
2018-12-02 15:44:54 +0100

    % cleanup day 2

--- src/main/kotlin/day02/Main.kt
+++ src/main/kotlin/day02/Main.kt
@@ -10,8 +10,8 @@ fun main(args: Array<String>) {
 fun getIds() = FileHelper.readFile("02.txt").filter { it.isNotEmpty() }
 
 fun partOne(): Int {
-    val twoOfAKind = getIds().filter { hasNumOfAKind(it, 2) }.count()
-    val threeOfAKind = getIds().filter { hasNumOfAKind(it,3) }.count()
+    val twoOfAKind = getIds().filter { hasExactlyNumOfAKind(it, 2) }.count()
+    val threeOfAKind = getIds().filter { hasExactlyNumOfAKind(it,3) }.count()
     println("2 of a kind: $twoOfAKind")
     println("3 of a kind: $threeOfAKind")
     return twoOfAKind * threeOfAKind
@@ -19,7 +19,7 @@ fun partOne(): Int {
 
 fun partTwo(): String {
     getIds().forEach { a ->
-        val dist = getIds().filter { b -> hammingDistance(a,b) == 1 }.firstOrNull()
+        val dist = getIds().firstOrNull { b -> hammingDistance(a,b) == 1 }
         dist?.let {
             println("Hamming distance between $a and $dist is 1.")
             return commonSubstring(a, dist)
@@ -32,8 +32,8 @@ fun commonSubstring(a: String, b: String): String {
     return a.zip(b).filter { it.first == it.second }.map { it.first }.joinToString("")
 }
 
-fun hasNumOfAKind(id: String, num: Int): Boolean {
-    return id.groupBy { it }.mapValues { vals -> vals.value.count() }.any { it.value == num }
+fun hasExactlyNumOfAKind(id: String, num: Int): Boolean {
+    return id.groupBy { it }.mapValues { it.value.count() }.any { it.value == num }
 }
 
 fun hammingDistance(a: String, b: String): Int {
