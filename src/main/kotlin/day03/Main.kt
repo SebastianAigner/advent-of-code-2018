a6a9e16c099d15a804db5b6d2dc0c9b70d124271
Sebastian Aigner <sebastian.aigner@tum.de>
2018-12-03 15:45:35 +0100

    + day 3 solution + day 3 README

---
+++ src/main/kotlin/day03/Main.kt
@@ -0,0 +1,70 @@
+package day03
+
+import filehelper.FileHelper
+
+fun main(args: Array<String>) {
+    println("Part One: ${partOne()}")
+    println("Part Two: ${partTwo()}")
+}
+
+fun parseInput(): List<Claim> {
+    val input = FileHelper.readFile("03.txt").filter { it.isNotEmpty() }
+    val destructuredRegex = "#(\\d+) @ (\\d+),(\\d+): (\\d+)x(\\d+)".toRegex()
+    return input.map {
+        destructuredRegex.matchEntire(it)?.destructured?.let {
+            (id, left, right, width, height) ->
+            Claim(id.toInt(), left.toInt(), right.toInt(), width.toInt(), height.toInt())
+        } ?: error("Malformatted input.")
+    }
+}
+
+fun partOne(): Int {
+    val claims = parseInput()
+    val sheet = Sheet()
+    claims.forEach {
+        sheet.claim(it)
+    }
+    return sheet.inchesMulticlaimed()
+}
+
+fun partTwo(): Int /* id */ {
+    val claims = parseInput()
+    val sheet = Sheet()
+    claims.forEach {
+        sheet.claim(it)
+    }
+    return claims.find { !sheet.checkHasOverlap(it) }?.id ?: error("No non-overlapping claim was found in input.")
+}
+
+data class Claim(val id: Int, val left: Int, val top: Int, val width: Int, val height: Int)
+
+class Sheet {
+    //x,y coordinates of the square, and the number of times it has been claimed
+    val claimedSquares = mutableMapOf<Pair<Int, Int>, Int>()
+    fun claim(c: Claim) {
+        for(x in 0 until c.width) {
+            for(y in 0 until c.height) {
+                val coords = Pair(x + c.left, y + c.top)
+                val claims = claimedSquares.getOrDefault(coords, 0) + 1
+                claimedSquares[coords] = claims
+            }
+        }
+    }
+
+    fun inchesMulticlaimed(): Int {
+        return claimedSquares.filterValues { it > 1 }.count()
+    }
+
+    fun checkHasOverlap(c: Claim): Boolean {
+        for(x in 0 until c.width) {
+            for(y in 0 until c.height) {
+                val coords = Pair(x + c.left, y + c.top)
+                val claims = claimedSquares[coords]
+                if(claims != 1) {
+                    return true
+                }
+            }
+        }
+        return false
+    }
+}
---
+++ src/main/kotlin/day03/README.md
@@ -0,0 +1,16 @@
+# Day 3
+(from https://adventofcode.com/2018/day/3)
+
+## Part 1 (abridged)
+`A claim like #123 @ 3,2: 5x4 means that claim ID 123 specifies a rectangle 3 inches from the left edge, 2 inches from the top edge, 5 inches wide, and 4 inches tall. How many square inches of fabric are within two or more claims?`
+
+### Approach
+The input is a little bit more complicated, which is a great excuse to use [Regex destructuring](https://medium.com/@garnop/safe-concise-text-parsing-with-regex-destructuring-in-kotlin-b8f77ef1e30c) to parse it into Kotlin objects.
+
+For each rectangle, we simply keep track of how many times it has been claimed on a sheet. I did this via a map. Finding the parts that are claimed multiple times is as easy as counting the number of map entries that have more than one claim.
+
+## Part 2 (abridged)
+`What is the ID of the only claim that doesn't overlap?`
+
+### Approach
+A claim has no overlap if every square of it has _exactly one_ claim on it. We can end a check prematurely if we realize that there is more than one claim on any of the squares outlined by the claim.
